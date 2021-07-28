653fa796c18630b7b3122a52094b05d928fdf7a5
David Konečný <david@konecny.eu>
2021-07-28 16:20:02 +0200

    feat: init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
+import type { AppProps } from "next/app";
+import React from "react";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
